28bc7145a85e7331854d692e62e418fd53569ba8
arunbhagat <arunbhagat@microsoft.com>
2025-10-17 11:15:01 +0530

    first api

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -6,9 +6,10 @@ public class Order {
     private int deliveryTime;
 
     public Order(String id, String deliveryTime) {
-
-        // The deliveryTime has to converted from string to int and then stored in the attribute
-        //deliveryTime  = HH*60 + MM
+        this.id = id;
+        String []arr = deliveryTime.split(":");
+        this.deliveryTime = (Integer.parseInt(arr[0]) * 60) +
+                (Integer.parseInt(arr[1]));
     }
 
     public String getId() {
@@ -16,4 +17,12 @@ public class Order {
     }
 
     public int getDeliveryTime() {return deliveryTime;}
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -18,11 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("orders")
 public class OrderController {
+    private final OrderService orderService;
 
+    @Autowired
+    OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
-
+        orderService.addOrder(order);
         return new ResponseEntity<>("New order added successfully", HttpStatus.CREATED);
     }
 
